4fdecce5a0de758b31598f55b4fe06b0f2a615cf
huyvu8051 <huyvu8051@gmail.com>
2022-03-18 14:44:14 +0700

    init project

---
+++ src/main/java/com/bobvu/tinherbackend/TinherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bobvu.tinherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinherBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobvu/tinherbackend/TinherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobvu.tinherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
