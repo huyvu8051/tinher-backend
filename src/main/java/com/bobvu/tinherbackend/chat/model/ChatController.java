abc9874fad039fa02bc468da8e3b1edc93b771ee
huyvu8051 <huyvu8051@gmail.com>
2022-03-20 15:27:55 +0700

    run project successfull

---
+++ src/main/java/com/bobvu/tinherbackend/chat/model/ChatController.java
@@ -0,0 +1,14 @@
+package com.bobvu.tinherbackend.chat.model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+    @GetMapping
+    public String home (){
+       return "hello chat ";
+    }
+}
